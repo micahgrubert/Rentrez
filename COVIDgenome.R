2a82da8112d7ac1c4e6e5e5ebed9c873796801f1
micahgrubert <16mdgv@queensu.ca>
2021-02-12 11:38:06 -0500

    cleaned up

--- COVIDgenome.R
+++ COVIDgenome.R
@@ -8,7 +8,9 @@ Seq<-gsub(" |//","",Seq) #remove spaces and //
 
 Seq<-gsub("([actg])","\\U\\1",Seq,perl=T) # all uppercase
 
-substr(Seq, start = 21563, stop = 25384) # isolate characters 21563-25384 in the string
+spike<-substr(Seq, start = 21563, stop = 25384) # isolate characters 21563-25384 in the string
+
+spike # show sequence
 
 # This gene appears to be incredibly highly conserved, with the most "different" variant still having a 99.92% sequence identity. 
 # Despite the hundred different variants that were compared, there was minimal variation in the coding sequence of this protein.
