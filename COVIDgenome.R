ecdb9cd5f0bc96bf0c0cd130247cf2f8285d26de
micahgrubert <16mdgv@queensu.ca>
2021-02-11 22:43:39 -0500

    not muched changed

--- COVIDgenome.R
+++ COVIDgenome.R
@@ -1,12 +1,15 @@
-covid<- scan("./sequence.gb",what="character",sep="\n")
+covid<- scan("./sequence.gb",what="character",sep="\n") #load Covid genome from genbank
 
-Seq<-gsub(".*1 *([gatc])","\\1",paste(covid))
+Seq<-gsub(".*1 *([gatc])","\\1",covid) # remove the index number in front of the sequence
 
-Seq<-gsub(".*ORIGIN +","",paste(Seq,collapse=""))
+Seq<-gsub(".*ORIGIN +","",paste(Seq,collapse="")) #remove everything before origin, where the sequence begins. Collapse down to one string.
 
-Seq<-gsub(" |//","",Seq)
+Seq<-gsub(" |//","",Seq) #remove spaces and //
 
-Seq<-gsub("([actg])","\\U\\1",Seq,perl=T)
+Seq<-gsub("([actg])","\\U\\1",Seq,perl=T) # all uppercase
 
-Seq<-gsub("(ATG([ATGC]{3})+?(TAA|TAG|TGA)+?)","-->\\1<--",Seq)
-Seq
+substr(Seq, start = 21563, stop = 25384) # isolate characters 21563-25384 in the string
+
+# This gene appears to be incredibly highly conserved, with the most "different" variant still having a 99.92% sequence identity. 
+# Despite the hundred different variants that were compared, there was minimal variation in the coding sequence of this protein.
+
